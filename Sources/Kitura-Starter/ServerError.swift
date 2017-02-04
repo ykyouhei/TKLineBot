a776c962b803dd3c07ccf015c0c015a7cdaac90e
ykyouhei <kyouhei.lab@gmail.com>
2017-02-04 18:47:16 +0900

    ObjectStorageSDKとそのManagerクラスを追加

---
+++ Sources/Kitura-Starter/ServerError.swift
@@ -0,0 +1,15 @@
+//
+//  ServerError.swift
+//  Kitura-Starter
+//
+//  Created by kyo__hei on 2017/02/04.
+//
+//
+
+import Foundation
+
+enum ServerError: Error {
+    
+    case emptyResponse
+    
+}
